8fbfb2b365838afd3276959fe9975de2be837590
c-daniel14 <cojocarugeani@yahoo.com>
2018-05-12 16:28:49 +0100

    improved navbar

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function menu() {
+	document.getElementById("menu").style.opacity = "0";
+}
+
+function menuRestore(){
+	document.getElementById("menu").style.opacity ="1";	
+}
