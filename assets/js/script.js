d60788530524b408a2f7998179c42f385c94d51e
c-daniel14 <cojocarugeani@yahoo.com>
2018-05-17 22:19:26 +0100

    upgrade

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const openSideMenu = () =>{
+	document.querySelector("#side-menu").style.width = "250px";
+	document.querySelector("#main").style.marginLeft = "250px";
+}
+
+const closeSideMenu = () =>{
+	document.querySelector("#side-menu").style.width = "0";
+	document.querySelector("#main").style.marginLeft = "0";
+}
