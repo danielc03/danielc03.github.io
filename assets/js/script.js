071fcea9c87e05240cb2139426bb725af321400d
Geani Daniel Cojocaru <cojocarugeani@yahoo.com>
2018-05-17 22:14:59 +0100

    Delete script.js

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-const openSideMenu = () =>{
-	document.querySelector("#side-menu").style.width = "250px";
-	document.querySelector("#main").style.marginLeft = "250px";
-}
-
-const closeSideMenu = () =>{
-	document.querySelector("#side-menu").style.width = "0";
-	document.querySelector("#main").style.marginLeft = "0";
-}
