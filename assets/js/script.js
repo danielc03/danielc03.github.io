c1b938c9fb59edbecd5d2c0cb1ef6604efb092e3
c-daniel14 <cojocarugeani@yahoo.com>
2018-06-16 15:37:07 +0100

    removed unnecessary code

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,25 +36,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-
-
-//  TOP BUTTON
-
-let marginY = 0;
-let speed = 10;
-let scroller = null;
-
-const toTop = (elementId) => {
-	destination = document.querySelector(`#${elementId}`).offsetTop;
-
-	scroller = setTimeout(function(){
-		toTop(elementId);
-	}, 1);
-	marginY -= speed;
-
-	if(marginY <= 0 ){
-		clearTimeout(scroller);
-	}
-
-	window.scroll(0, marginY);
-};
