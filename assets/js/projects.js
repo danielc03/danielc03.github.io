c433229df60e322a604c2a72c6db7f28d5f9a1c7
c-daniel14 <cojocarugeani@yahoo.com>
2018-08-12 14:33:48 +0100

    changing projects

---
+++ assets/js/projects.js
@@ -0,0 +1,12 @@
+github = document.querySelector('#github-finder');
+
+
+
+// Event listener
+// github.addEventListener('mouseover', () => {
+//     document.getElementById('github-description').innerHTML = `Created with Bootstrap4<br>Pure JavaScript and GitHub users API<br>
+//     Find users on Github - Enter the username in the search-box for the profile to display with the latest repositories`;
+// })
+// taskList.addEventListener('mouseout', () => {
+//     document.getElementById('task-description').innerHTML = ``;
+// })
